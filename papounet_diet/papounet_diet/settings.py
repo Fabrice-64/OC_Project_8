f4762a6f402f8ad7812a93ed1e84fa9d709b6e38
Fabrice-64 <fab.jaouen@gmail.com>
2020-10-12 21:48:06 +0200

    Basic set up django & postgresql

--- papounet_diet/papounet_diet/settings.py
+++ papounet_diet/papounet_diet/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'papounet_diet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'papounet_diet',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
