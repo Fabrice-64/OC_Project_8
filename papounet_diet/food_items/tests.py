8b3ae9f789dfb3d31ddff6ab41959866eab7334e
Fabrice-64 <fab.jaouen@gmail.com>
2020-10-17 21:10:02 +0200

    Implement first test: connection to Open Food Facts

--- papounet_diet/food_items/tests.py
+++ papounet_diet/food_items/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+import requests
+import unittest
 
-# Create your tests here.
+
+class TestOpenFoodFacts(TestCase):
+    def test_connection_ok(self):
+        r = requests.get('https://fr.openfoodfacts.org/cgi/search.pl?search_simple=1')
+        self.assertEqual(r.status_code, 200)
+
+    def test_download_static(self):
+        pass
+
+    def test_download_items(self):
+        pass
