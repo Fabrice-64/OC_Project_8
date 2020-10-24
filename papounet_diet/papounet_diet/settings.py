046b88c58f0345c29ad7d698b6d04458698a5b54
Fabrice-64 <fab.jaouen@gmail.com>
2020-10-24 15:14:32 +0200

    Implement environment variable feature

--- papounet_diet/papounet_diet/settings.py
+++ papounet_diet/papounet_diet/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'twml)5p3dz8$m=__%9k59iuk@m&#w=z78i8k@&^6ugoyhe8i%$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
