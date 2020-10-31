c157134472a7bb557e5b27e15dbb478010378612
Fabrice-64 <fab.jaouen@gmail.com>
2020-10-31 21:41:28 +0100

    Fields page home.html : OK Tests fields displayed on page home.html : OK Pictures/icons: NOK Inputs: NOK Design: NOK

--- papounet_diet/papounet_diet/tests.py
+++ papounet_diet/papounet_diet/tests.py
@@ -48,25 +48,40 @@ class CustomerTestCase(LiveServerTestCase):
         # In the upper half of the page, a picture
         self.assertIsNotNone(self.browser.find_element_by_css_selector(
             'img#background_picture'))
-        # Contains a piece of advice
-        motto = self.assertIs(self.browser.find_element_by_css_selector(
-            'h1#motto'))
-        # and a search field and a validation button.
-        self.assertContains(motto.text, "Pur Beurre, la qualité")
+        # Contains the motto and a piece of advice
+        self.browser.find_element_by_css_selector('h1#motto')
+        self.browser.find_element_by_css_selector('h2#advice')
+        # and a search field with a validation button.
+        main_customer_input = self.browser.find_element_by_css_selector(
+            'input#main_customer_input')
+        self.assertEqual(main_customer_input.get_attribute(
+            'placeholder'), 'Recherche')
         # Just below Lily Kala can see the story of the company
-
+        self.browser.find_element_by_css_selector('h2#heroes')
+        self.browser.find_element_by_css_selector('p#story')
         # With the pictures of Colette and Remy
-
+        self.assertIsNotNone(self.browser.find_element_by_css_selector(
+            'img#Colette'))
+        self.assertIsNotNone(self.browser.find_element_by_css_selector(
+            'img#Remy'))
         # Still below LK sees an invite to contact the company
-
+        call_to_action = self.browser.find_element_by_css_selector(
+            'h2#call_to_action')
         # Per telephone
-
+        self.assertIsNotNone(self.browser.find_element_by_css_selector(
+            'img#telephone'))
+        self.assertIsNotNone(self.browser.find_element_by_css_selector(
+            'p#phone_number'))
         # Or per e-mail
-
+        self.assertIsNotNone(self.browser.find_element_by_css_selector(
+            'img#e_mail'))
+        self.assertIsNotNone(self.browser.find_element_by_css_selector(
+            'p#e_mail'))
         # At the bottom of the page, she can find the terms of reference
         self.assertIsNotNone(self.browser.find_element_by_css_selector(
             'a#terms_of_use'))
-        self.assertIsNotNone(self.browser.find_element_by_css_selector('a#contact'))
+        self.assertIsNotNone(self.browser.find_element_by_css_selector(
+            'a#contact'))
 
         self.fail("Test Incomplet")
 
