f1336609cf0854ed65e98798b72ed46a9d05db35
Fabrice-64 <fab.jaouen@gmail.com>
2020-11-17 22:05:13 +0100

    Start implementation of authentication

--- papounet_diet/customer/urls.py
+++ papounet_diet/customer/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -6,6 +7,7 @@ app_name = "customer"
 urlpatterns = [
     path('', views.home, name="home"),
     path('register/', views.register, name="register"),
-    path('login/', views.login, name="login"),
-    path('logout/', views.logout, name="logout"),
+    path('login/', views.user_login, name="login"),
+    path('logout/', auth_views.LogoutView.as_view, name="logout"),
+    path('login/', auth_views.LoginView.as_view(), name="login"),
 ]
