916cca61a064eed2f354009f6a62aab775891332
Fabrice-64 <fab.jaouen@gmail.com>
2020-12-07 20:52:42 +0100

    SetUp mock_data stores & categories

---
+++ papounet_diet/food_items/openfoodfacts/tests/mock_data.py
@@ -0,0 +1,23 @@
+import os
+import json
+
+current_path = os.path.abspath(os.getcwd())
+def get_store_mock_data(self, mock_data):
+    with open(os.path.join(current_path, "food_items/openfoodfacts/tests/off_data_to_be_tested/", mock_data), 'r') as f:
+        mock_data = json.load(f)
+    return mock_data
+
+class MockDataOFF:
+    def __init__(self):
+        self.current_path = os.path.abspath(os.getcwd())
+
+    def get_mock_data(self, mock_data):
+        with open(os.path.join(current_path, "food_items/openfoodfacts/tests/off_data_to_be_tested/", mock_data), 'r') as f:
+            mock_data = json.load(f)
+        return mock_data
+
+    store_data = get_mock_data("mock_stores.json")
+
+    category_data = get_mock_data("mock_categories.json")
+
+    
